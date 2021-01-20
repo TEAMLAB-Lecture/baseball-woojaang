5dc66100e4f5ffb82323a4d7e839176699e1b321
woojaang <woojaang@naver.com>
2021-01-20 17:43:50 +0900

    update copde

--- baseball_game.py
+++ baseball_game.py
@@ -280,21 +280,24 @@ def is_no(one_more_input):
 
 
 def main():
-    print("Play Baseball")
-    
-    random_number = str(get_not_duplicated_three_digit_number())
-    print("Random Number is : ", random_number)
     # ===Modify codes below=============
     # 위의 코드를 포함하여 자유로운 수정이 가능함
     while True:
-        user_input = input("Input guess number : ")
-        
-        if is_validated_number(user_input) == False:
-            if user_input == '0':
-                break
+        print("Play Baseball")
+        random_number = str(get_not_duplicated_three_digit_number())
+        print("Random Number is : ", random_number)
+        while True:
+            user_input = input("Input guess number : ")
+            if is_validated_number(user_input) == False:
+                if user_input == '0':
+                    break
+                else:
+                    print("Wrong Input")
+                    continue
             else:
-                print("Wrong Input")
-                continue
+                break
+        if user_input == '0':
+            break
         score = get_strikes_or_ball(user_input, random_number)
         print(f"Strikes : {score[0]}, Balls : {score[1]}")
         if score == [3, 0]:
@@ -310,16 +313,9 @@ def main():
                 else:
                     print("Wrong Input")
                     continue
-            if replay == 1:
-                main()
-            else:
+            if replay == 0:
                 break
-
-
-
-
-
-
+        
     # ==================================
     print("Thank you for using this program")
     print("End of the Game")
